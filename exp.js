045506bf1f26bfceabe3c0fec3baa6be7d51b49d
Nitish Kumar <stnitish01@gmail.com>
2024-09-04 19:04:57 +0530

    Update exp.js

--- exp.js
+++ exp.js
@@ -35,7 +35,7 @@ var Tawk_API = Tawk_API || {},
   var s1 = document.createElement("script"),
     s0 = document.getElementsByTagName("script")[0];
   s1.async = true;
-  s1.src = "https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6";
+  s1.src='https://embed.tawk.to/66d80ae750c10f7a00a3cc36/1i6tul6gt';
   s1.charset = "UTF-8";
   s1.setAttribute("crossorigin", "*");
   s0.parentNode.insertBefore(s1, s0);
