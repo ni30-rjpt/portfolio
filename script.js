667b1db3788b6eec5e1f49245407eb4768e2aa49
Nitish Kumar <stnitish01@gmail.com>
2024-09-05 15:41:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function showProjects(projects) {
     .forEach((project) => {
       projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
